93980cbcdcb7718ba301c981d68c73f2ef2d0a0a
Thomas Scrivner <Thomas.M.Scrivner-1@ou.edu>
2023-12-02 22:02:35 -0600

    initial commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,113 +1,205 @@
-import Image from 'next/image'
+"use server";
+import { Suspense } from 'react';
+import {
+        Card,
+        Grid,
+        Tab,
+        TabGroup,
+        TabList,
+        TabPanel,
+        TabPanels,
+        Text,
+        Title,
+        Table,
+        TableHead,
+        TableHeaderCell,
+        TableBody,
+        TableRow,
+        TableCell,
+        BarChart,
+        
+      } from "@tremor/react";
 
-export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+function useFetchData(url) {
+        const [data, setData] = useState(null);
+      
+        useEffect(() => {
+          fetch(url)
+            .then((res) => res.json())
+            .then((data) => setData(data))
+            .catch((err) => console.log(`Error: ${err}`));
+        }, [url]);
+        
+        return { data };
 }
+
+      function changeBackgroundColor(index) {
+        if (index==0) {
+          return '#ADD8E6'
+        }
+        if (index == 1) {
+          return '#77dd77'
+        }
+        if (index == 2) {
+          return '#ffb347'
+        }
+      }
+
+      function abbrev(index) {
+        if (index == 0) {
+          return 'LLL'
+        } 
+      
+        if (index == 1) {
+          return 'MMM'
+        }
+      
+        if (index == 2) {
+          return 'WWW'
+        }
+      }
+
+      function countRank(cnt) {
+        if (cnt == 10) {
+          cnt = 1;
+        }
+      
+        return cnt+1;
+      }
+      
+      function calculateWinPct(wins,losses,ties,points) {
+        return (2 *  wins + ties) / (2 *  wins+losses+ties) * 100 * points;
+      }
+
+      
+
+async function getData() {
+  //await new Promise(resolve => setTimeout(resolve,3000))
+
+  const res = await fetch("https://fnfantasyfootball.uc.r.appspot.com/data");
+
+  return res.json();
+}   
+
+
+function valueFormatter(number) {
+  return `$ ${new Intl.NumberFormat("us").format(number).toString()}`;
+}
+
+export default async function Home() {
+
+  const data = await getData();
+  console.log(data);
+
+
+  function changeColor(topPts,bottomPts) {
+    if (topPts > bottomPts) {
+      return 'green';
+    }
+  
+    else if (topPts < bottomPts) {
+      return 'red';
+    }
+    
+    else{
+      return 'black'
+    }
+  }
+
+  return (
+
+    <main className="p-12">
+      <Title><strong>FN Central Region Fantasy League</strong></Title>
+      <Text>Demo Dashboard using Fantasy Football Data</Text>
+      
+      <TabGroup className="mt-6">
+        <TabList>
+          <Tab>Overview</Tab>
+          <Tab>Props</Tab>
+        </TabList>
+        <TabPanels>
+          <TabPanel>
+            <Grid numItemsMd={2} numItemsLg={3} className="gap-6 mt-6">
+              <Suspense fallback={<Loading />}>
+              {data && (data.league_list.map((league_list,index) => (
+              
+              <Card key={league_list.name} style={{backgroundColor:changeBackgroundColor(index)}}>
+                <Title>{league_list.name}</Title>
+                
+                <Table>
+                  <TableHead>
+                    <TableRow>
+                      <TableHeaderCell>Rank</TableHeaderCell>
+                      <TableHeaderCell>Name</TableHeaderCell>
+                      <TableHeaderCell>PF</TableHeaderCell>
+                      <TableHeaderCell>W/L/T</TableHeaderCell>
+                    </TableRow>
+                  </TableHead>
+                  <TableBody>
+                  {league_list.player_list.sort((a,b) => calculateWinPct(b.wins,b.losses,b.ties,b.points) - calculateWinPct(a.wins,a.losses,a.ties,a.points) ).map((player_list,index) => (
+                    <TableRow key={player_list.name}>
+                      <TableCell>
+                        {index + 1}
+                      </TableCell>
+                      <TableCell>
+                        {player_list.name}
+                      </TableCell>
+                      <TableCell>
+                        {player_list.points.toFixed(2)}
+                      </TableCell>
+                      <TableCell>
+                        {player_list.wins}-{player_list.losses}-{player_list.ties}
+                      </TableCell>
+                      
+                    </TableRow>
+
+                  ))}
+                  </TableBody>
+
+                </Table>
+              </Card>)))}
+              </Suspense>
+              
+            </Grid>
+            <Grid numItemsLg={1} className="gap-6 mt-6">
+          
+              {data && (data.league_list.map((league_list,index) => (
+                <Card key={league_list.name} style={{backgroundColor:changeBackgroundColor(index)}}>
+                <Title>{league_list.name}</Title>
+                <BarChart className="mt-6"
+                    data={league_list.player_list}
+                    index="name"
+                    categories={["points"]}
+                    colors={["blue"]}
+                    yAxisWidth={48}
+
+                    />
+              </Card>
+              )))}
+            
+            
+            
+            </Grid>
+            
+          </TabPanel>
+          <TabPanel>
+            <div className="mt-6">
+              <Card>
+                <div className="h-96" />
+              </Card>
+            </div>
+          </TabPanel>
+        </TabPanels>
+      </TabGroup>
+    </main>
+  );
+}
+
+
+function Loading() {
+  // You can add any UI inside Loading, including a Skeleton.
+  return <h2>🌀 Loading...</h2>;
+
+  }
